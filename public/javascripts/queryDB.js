c5b328d882b5bc77aa19fb17e85873623122b397
Andrew Boxall <42051071+AndrewBoxall@users.noreply.github.com>
2019-07-08 15:34:53 +1200

    Add files via upload

---
+++ public/javascripts/queryDB.js
@@ -0,0 +1,5 @@
+export default function queryDB(collection, param){
+  //var element = getElementById('event');
+  mongoose.connect(`mongodb+srv://Andrew:<password>@cluster0-gbspm.mongodb.net/test?retryWrites=true`);
+  return null;
+}
