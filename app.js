244a7a7db148ec07e14e59bb7cbdb9d35f53eccc
Andrew Boxall <42051071+AndrewBoxall@users.noreply.github.com>
2019-07-09 15:29:56 +1200

    Add files via upload

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.sendFile(path.join(__dirname, '../public/portfolio/build', 'index.html'));
+  //res.sendFile(path.join(__dirname, '../public/portfolio/build', 'index.html'));
   //res.render('error');
 });
 
